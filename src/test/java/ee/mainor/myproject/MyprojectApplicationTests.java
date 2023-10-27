ee0e23b1c54c4428d6e4b7f366644edbfd4d5193
Carlang77 <103078964+Carlang77@users.noreply.github.com>
2023-10-27 16:40:03 +0100

    first commit

---
+++ src/test/java/ee/mainor/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.mainor.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
