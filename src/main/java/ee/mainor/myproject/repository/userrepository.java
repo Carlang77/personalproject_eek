a156e7996d7de05c2c186bdc117aa6ea0bc07a93
Carlang77 <103078964+Carlang77@users.noreply.github.com>
2023-11-11 21:54:59 +0100

    Completed model and repository package.
    Signed-off-by: Carlang77 <103078964+Carlang77@users.noreply.github.com>

---
+++ src/main/java/ee/mainor/myproject/repository/userrepository.java
@@ -0,0 +1,10 @@
+package ee.mainor.myproject.repository;
+import ee.mainor.myproject.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserModel, Integer> {
+
+    Optional<UserModel> findByLoginAndPassword(String login, String password);
+
+}
---
+++ src/main/java/ee/mainor/myproject/service/UserService.java
@@ -0,0 +1,12 @@
+package ee.mainor.myproject.service;
+
+import ee.mainor.myproject.model.UserModel;
+import ee.mainor.myproject.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+
+}
