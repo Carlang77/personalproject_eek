f6e85f2219a4a4847f4a17b24d45afc4590a09e9
Carlang77 <103078964+Carlang77@users.noreply.github.com>
2023-11-11 16:56:01 +0100

    Corrected error. Moved model class from test to main. Added repository folder.
    Signed-off-by: Carlang77 <103078964+Carlang77@users.noreply.github.com>

---
+++ src/main/java/ee/mainor/myproject/model/UserModel.java
@@ -0,0 +1,76 @@
+package ee.mainor.myproject.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity //Mark the class as a JPA (Java Persistence API) entity.
+@Table(name = "user_table")//Class instances will be stored in a table named "user_table."
+
+
+public class UserModel {
+
+    @Id // Will use to declare the primary key field of the table.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id; //defines primary.
+
+    String login;
+    String password;
+    String email;
+
+//Create getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return Objects.equals(id, userModel.id) && Objects.equals(login, userModel.login) && Objects.equals(password, userModel.password) && Objects.equals(email, userModel.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password, email);
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
